6ce94efa7c8860fdb5d3e89d02a03485a3ed874e
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-02-07 15:18:20 +0000

    Completed Problems

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class PracticeProblem {
-
+	static Scanner sc;
 	public static void main(String args[]) {
 		q1();
 		q2();
@@ -11,34 +11,46 @@ public class PracticeProblem {
 		q6();		
 	}
 
-	public static void q1() {
-		//Write question 1 code here
-		
+	public static void q1(){
+		sc = new Scanner(System.in);
+		System.out.print("Input an integer: ");
+		String num = sc.nextLine();
+		System.out.println(Integer.parseInt("5" + num) + 5);
 	}
 
-	public static void q2() {
-		//Write question 2 code here
-		
+	public static void q2(){
+		sc = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		String num = sc.nextLine();
+		System.out.println(Double.parseDouble("4" + num + "3") + 3.4);
 	}
 
-	public static void q3() {
-		//Write question 3 code here
-		
+	public static void q3(){
+		sc = new Scanner(System.in);
+		System.out.print("Input a boolean: ");
+		String bool = sc.nextLine();
+		System.out.println(!Boolean.parseBoolean(bool));
 	}
 
-	public static void q4() {
-		//Write question 4 code here
-		
+	public static void q4(){
+		sc = new Scanner(System.in);
+		System.out.print("Input an integer: ");
+		String num = sc.nextLine();
+		System.out.println((char)(Integer.parseInt(num + "3") + 2));
 	}
 
-	public static void q5() {
-		//Write question 5 code here
-		
+	public static void q5(){
+		sc = new Scanner(System.in);
+		System.out.print("Input an integer: ");
+		String num = sc.nextLine();
+		System.out.println((double)((Integer.parseInt(num + "1"))/2));
 	}
 
-	public static void q6() {
-		//Write question 6 code here
-		
+	public static void q6(){
+		sc = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		String num = sc.nextLine();
+		System.out.println((int)(Double.parseDouble("1" + num)));
 	}
 
 }
